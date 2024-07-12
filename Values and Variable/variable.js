d04e2a8a4231f52958a5067a83fffd14a0721e0f
Rohan Rai <109414504+irohanrai@users.noreply.github.com>
2024-07-12 12:41:09 +0530

    Add files via upload

---
+++ Values and Variable/variable.js
@@ -0,0 +1,8 @@
+var my_Name = 'Rohan';
+var last_Name = 'Rai';
+// var 123age = '24';
+var $age = '24';
+// var my@email = 'Rohan'; 
+console.log(my_Name);
+console.log(last_Name);
+console.log($age);
